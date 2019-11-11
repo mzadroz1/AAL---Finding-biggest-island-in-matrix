7b1f3a33d3751ebf93459e371ccdba8646db879e
Michal <michalzadrozny19@gmail.com>
2019-11-11 18:05:40 +0100

    Bfs_works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        Matrix m = new Matrix(100,200);
-        //m.printMatrix();
-        long start = System.currentTimeMillis();
+        Matrix m = new Matrix(6,8);
+        m.printMatrix();
+        //long start = System.currentTimeMillis();
         System.out.println(m.findBiggestIslandBFS());
-        long end = System.currentTimeMillis();
-        System.out.println((end - start) + "ms");
+       // long end = System.currentTimeMillis();
+       // System.out.println((end - start) + "ms");
     }
 }
